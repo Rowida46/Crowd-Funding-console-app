cacd3044798c738d30c9bfdc459a5fc7711bad82
Rowida46 <rowidanagah462000@gmail.com>
2023-02-14 16:50:38 +0200

    registration done

--- helpers/db_handler.py
+++ helpers/db_handler.py
@@ -8,11 +8,18 @@ users_file = f"{dir}/{users_file}"
 with open(users_file) as json_file:
     users_data = json.load(json_file)
 
+
+def write_users(data, filename=users_file):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
 def insert_user(user):
     print("inside insert")
     if is_file(users_file):
-        with open(users_file) as json_file:
+        with open(users_file, "r+") as  json_file:
             users_data = json.load(json_file)
-            print(users_data)
             users_data.append(user)
+            json_file.seek(0)
+            json.dump(users_data, json_file)
+
     
